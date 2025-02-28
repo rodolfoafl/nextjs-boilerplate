000aceb89e3ba7bb4b3b5c48ac52764f66588fb5
rodolfoafl <27327749+rodolfoafl@users.noreply.github.com>
2025-02-28 00:50:48 -0300

    chore: initial project setup 3

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,6 +1,6 @@
-module.exports = {
-  "*.{js,jsx,ts,tsx}": (filenames) => [
+export default {
+  '*.{js,jsx,ts,tsx}': (filenames) => [
     `prettier --write ${filenames.join(' ')}`,
     `npm run lint --fix . ${filenames.join(' --file')}`
   ]
-};
+}
