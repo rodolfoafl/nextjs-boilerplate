137969b0bbb43443d933b104e7c840671f3e17fa
rodolfoafl <27327749+rodolfoafl@users.noreply.github.com>
2025-02-28 11:03:05 -0300

    chore: removed unused css

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -1,7 +1,7 @@
 export default function Main() {
   return (
-    <main className="bg-color-[#06092b] text-white w-full h-full p-12 text-center flex flex-col items-center justify-center">
-      <h1>Main Component</h1>
+    <main className="bg-[#06092b] text-white w-full h-full p-12 text-center flex flex-col items-center">
+      <h1 className="text-5xl">Main Component</h1>
     </main>
   )
 }
