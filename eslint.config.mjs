9de0140e3b7f3a9a63126c5c9c40f767d7765c0a
rodolfoafl <27327749+rodolfoafl@users.noreply.github.com>
2025-02-28 00:48:52 -0300

    chore: initial project setup 3

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,8 +3,8 @@ import pluginJs from '@eslint/js'
 import tseslint from 'typescript-eslint'
 import pluginReact from 'eslint-plugin-react'
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
-import reactHooks from "eslint-plugin-react-hooks";
- 
+import reactHooks from 'eslint-plugin-react-hooks'
+
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
@@ -12,16 +12,18 @@ export default [
   {
     settings: {
       react: {
-        version: "detect"
+        version: 'detect'
       }
     }
   },
+  eslintPluginPrettierRecommended,
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
   {
     plugins: {
       'react-hooks': reactHooks
     },
-  },
-  {
     rules: {
       'react-hooks/rules-of-hooks': 'error',
       'react-hooks/exhaustive-deps': 'warn',
@@ -29,9 +31,5 @@ export default [
       'react/react-in-jsx-scope': 'off',
       '@typescript-eslint/explicit-module-boundary-types': 'off'
     }
-  },
-  eslintPluginPrettierRecommended,
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended
+  }
 ]
